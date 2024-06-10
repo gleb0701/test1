f855940154af426cb086091e15ee31fe0eea0eea
GlebAstafurov <astafurov.gleb@yandex.ru>
2024-06-10 13:07:04 +0300

    added source

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 
 int main() {
     cout << "Hello, World!" << endl;
     cout << "Bye-bye, World!" << endl;
+
+    ifstream file("source1.txt");
+    string s;
+    file >> s;
+
+    cout << s << endl;
 
     return 0;
 }
